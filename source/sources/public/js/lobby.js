0f8123d912cd6b4e9df636a581c55c9ba6f38e57
dattt-pixelz <82146551+dattt-pixelz@users.noreply.github.com>
2022-03-27 14:20:23 +0700

    Revert "random stone when play single mode"
    This reverts commit 781b5441b0c068200b4097b2e054a8d82d55a55a.

--- source/sources/public/js/lobby.js
+++ source/sources/public/js/lobby.js
@@ -192,13 +192,8 @@ $('document').ready(function () {
     });
 
     $('#btn_singleplayer').on('click', function () {
-        var stoneArray = [
-            'time',
-            'mind',
-            'power'
-        ];
-        var randomNumber = Math.floor(Math.random()*stoneArray.length);
-        websocket.emit('initiatesingleplayer', {stone: stoneArray[randomNumber]});
+        var stone = $('#stone-value').children("option:selected").val();
+        websocket.emit('initiatesingleplayer', {stone: stone});
     });
 
     $('#btn_leftgame').on('click', function (e) {
