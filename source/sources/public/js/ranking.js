99d7335729560e987afcc59e4c809b925baac145
Hoang Thai Hoc <hoc@creativeforce.io>
2022-03-27 14:03:00 +0700

    Ranking

--- source/sources/public/js/ranking.js
+++ source/sources/public/js/ranking.js
@@ -3,12 +3,13 @@ function getRanking() {
     if (data && data.data) {
       var r = data.data[0];
       var template = '';
+      template += '<tr><thead><td>Username</td><td>Score</td></thead></tr>';
       if (Array.isArray(r)) {
         for (let u of r) {
-          template += '<li><span class="username">' + u.username_won + '</span><span class="score">' + u.score + '</span></li>'
+          template += '<tr><td class="username">' + u.username_won + '</td><td class="score">' + u.score + '</td></tr>'
         }
       }
-      $('#ranking').html('<ul class="ranking">' + template + '</ul>');
+      $('#ranking').html('<table class="ranking-table">' + template + '</table>');
     }
   });
 }
