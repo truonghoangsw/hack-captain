2ebb45ed557c1e779af6e95b5d2958a180ced0ee
Hoang Thai Hoc <hoc@creativeforce.io>
2022-03-27 11:46:33 +0700

    Ranking page

--- source/sources/public/js/ranking.js
+++ source/sources/public/js/ranking.js
@@ -1,8 +1,19 @@
 function getRanking() {
-
+  $.get("/ranking", function(data){
+    if (data && data.data) {
+      var r = data.data[0];
+      var template = '';
+      if (Array.isArray(r)) {
+        for (let u of r) {
+          template += '<li><span class="username">' + u.username_won + '</span><span class="score">' + u.score + '</span></li>'
+        }
+      }
+      $('#ranking').html('<ul class="ranking">' + template + '</ul>');
+    }
+  });
 }
 
 $('document').ready(function () {
-
+  getRanking();
 });
 
